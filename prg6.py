9894ca49bcfb9c99248d34b80d5536d082920a6f
Gaurav-Kattimani <gaurav.kat024@gmail.com>
2025-11-20 21:41:26 +0530

    save the file

---
+++ prg6.py
@@ -0,0 +1,13 @@
+import sys
+if len(sys.argv) == 1:
+    text = "Radar"   
+else:
+    text = sys.argv[1]
+    
+text_lower = text.lower()
+reversed_text = text_lower[::-1]
+
+if text_lower == reversed_text:
+    print(f"'{text}' is a palindrome.")
+else:
+    print(f"'{text}' is not a palindrome.")
